feefb5094d21127042f6f3a32bac9e6130ab56e9
aandreaamaya <andreauxue@ciencias.unam.mx>
2024-03-22 19:15:21 -0600

    Integración de Bootstrap, Formulario categoría,
    Se agregó un modal de bootstrap, un botón “Nueva categoría, se declaró una variable global de JQuery y también  un formulario, y se definió un formulario con dos inputs.

--- src/app/module/product/component/category/category.component.ts
+++ src/app/module/product/component/category/category.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Category } from '../../_model/category';
 import { CategoryService } from '../../_service/category.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from'sweetalert2';
+
+declare var $: any; // JQuery
 
 @Component({
   selector: 'app-category',
@@ -10,9 +14,15 @@ import { CategoryService } from '../../_service/category.service';
 export class CategoryComponent {
 
   categories: Category[] = [];
-
+// Category form
+  form = this.formBuilder.group({
+    category: ["", [Validators.required]],
+    code: ["", [Validators.required]],
+});
+  submitted = false; // Form submitted
   constructor(
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private formBuilder: FormBuilder,
   ){
   }
 
@@ -20,8 +30,48 @@ export class CategoryComponent {
     this.getCategories();
   }
 
+  onSubmit(){
+    // validate form
+    this.submitted = true;
+    if(this.form.invalid) return;
+    this.submitted = false;
+
+    // add category to category list
+    let id = this.categories.length + 1;
+    let category = new Category(id, this.form.controls['category'].value!, this.form.controls['code'].value!, 1);
+    this.categories.push(category);
+
+    // close modal
+    this.hideModalForm();
+
+    // show message
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      toast: true,
+      text: 'La categoría se ha registrado con éxito',
+      background: '#D8D8D8',
+      showConfirmButton: false,
+      timer: 2500
+    });
+
+  }
+
   getCategories(){
     this.categories = this.categoryService.getCategories();
     console.log(this.categories);
   }
-}
+  getRegions(){
+    this.categories = this.categoryService.getCategories();
+  }
+
+showModalForm(){
+    $("#modalForm").modal("show");
+    this.form.reset();
+    this.submitted = false;
+  }
+
+hideModalForm(){
+    $("#modalForm").modal("hide");
+  }
+} 
--- src/app/module/product/product.module.ts
+++ src/app/module/product/product.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './component/category/category.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ProductModule { }
